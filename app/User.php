97c359e851cc79105f81349b6f8a35ddb3486602
ib <ib3.bluesky@gmail.com>
2020-12-12 05:54:04 +0900

    passwordreset

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
